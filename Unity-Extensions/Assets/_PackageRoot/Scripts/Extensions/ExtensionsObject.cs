f95482c0f69cfe025342929313c9174e3467f64f
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2022-03-14 08:31:14 +0200

    Added IsDestroyed check

--- Unity-Extensions/Assets/_PackageRoot/Scripts/Extensions/ExtensionsObject.cs
+++ Unity-Extensions/Assets/_PackageRoot/Scripts/Extensions/ExtensionsObject.cs
@@ -13,6 +13,15 @@ namespace System
             return (type.IsValueType & type.IsPrimitive);
         }
 
+        public static bool IsDestroyed(this Object instance)
+        {
+            // UnityEngine overloads the == opeator for the GameObject type
+            // and returns null when the object has been destroyed, but 
+            // actually the object is still there but has not been cleaned up yet
+            // if we test both we can determine if the object has been destroyed.
+            return instance == null && !ReferenceEquals(instance, null);
+        }
+
         public static Object Copy(this Object originalObject)
         {
             return InternalCopy(originalObject, new Dictionary<Object, Object>(new ReferenceEqualityComparer()));
---
+++ Unity-Extensions/Assets/_PackageRoot/Scripts/Extensions/ExtensionsUnityObject.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityEngine
+{
+    public static class ExtensionsUnityObject
+    {
+        public static bool IsDestroyed(this Object instance)
+        {
+            // UnityEngine overloads the == opeator for the GameObject type
+            // and returns null when the object has been destroyed, but 
+            // actually the object is still there but has not been cleaned up yet
+            // if we test both we can determine if the object has been destroyed.
+            return instance == null && !ReferenceEquals(instance, null);
+        }
+    }
+}
